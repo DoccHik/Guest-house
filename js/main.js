86263fa8f06ddde02fc3d71ba8a9c0612e5ce597
DoccHik <evgeniy161102@gmail.com>
2022-03-19 16:05:59 +0300

    new code

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.block-quests-carousel').slick({
+    arrows: false,
+    infinite: false,
+    speed: 1000,
+    slidesToShow: 1,
+    // centerMode: true,
+    variableWidth: true,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+});
